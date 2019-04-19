06614b49839676cb2b6b44337e849c4927926d76
WuLC <liangchaowu5@gmail.com>
2019-04-19 13:05:32 +0800

    receive POST request from github with custom http server

--- hook_server.py
+++ hook_server.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
+# Created on Fri Apr 19 2019 13:3:3
+# Author: WuLC
+# EMail: liangchaowu5@gmail.com
+
 from flask import Flask, jsonify
 import subprocess
 
 app = Flask(__name__)
 
-# Load the model
 @app.route('/github_hook', methods=['POST'])
 def rebuild():
     print('new commits to github repository')
