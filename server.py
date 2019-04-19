f278782a7b1b0a9bedb946dc7804e31a9831ad6f
WuLC <liangchaowu5@gmail.com>
2019-04-19 13:05:02 +0800

    trigger auto build

--- server.py
+++ server.py
@@ -1,27 +1,25 @@
 # coding: utf-8
-import json
 import pickle
 
-import pandas as pd
-import numpy as np
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 # Load the model
-model = pickle.load(open('model.pkl','rb'))
-labels ={
+model = pickle.load(open('model.pkl', 'rb'))
+labels = {
   0: "versicolor",   
   1: "setosa",
   2: "virginica"
 }
 
-@app.route('/api',methods=['POST'])
+
+@app.route('/api', methods=['POST'])
 def predict():
     # Get the data from the POST request.
-    data = request.get_json(force=True)
+    data = request.get_json(force = True)
     predict = model.predict(data['feature'])
     return jsonify(predict[0].tolist())
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug = True, host = '0.0.0.0')
